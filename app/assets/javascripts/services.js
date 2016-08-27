cef4a14996e5a3c1c2acf6eb65fa4a4c19a05f35
jbyall <jpb5002@gmail.com>
2016-08-27 11:59:35 -0600

    working example form

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,19 +1,45 @@
-function updateTags(){
-	  	var c1 = $("#cat1").val();
-	  	var c2 = $("#cat2").val();
-	  	var c3 = $("#cat3").val();
-	  	var c4 = $("#cat4").val();
-	  	var c5 = $("#cat5").val();
+function updateCategoryTags(){
+	  	var c1 = "Towing";
+	  	var c2 = "Roadside Assistance";
+	  	var c3 = "Inspection";
+	  	var c4 = "Repairs";
+	  	var c5 = "Maintenance";
+
+	  	if($("#cat1").val()){
+	  		c1 = $("#cat1").val();
+	  	}
+	  	if($("#cat2").val()){
+	  		c2 = $("#cat2").val();
+	  	}
+	  	if($("#cat3").val()){
+	  		c3 = $("#cat3").val();
+	  	}
+	  	if($("#cat4").val()){
+	  		c4 = $("#cat4").val();
+	  	}
+	  	if($("#cat5").val()){
+	  		c5 = $("#cat5").val();
+	  	}
+
 	  	$("#tags-ex").text(c1 + " | " + c2 + " | " + c3 + " | " + c4 + " | " + c5);
 	  };
 function updateLocation(){
-		var city = $("#service_contact_city").val();
-	  	var state = $("#service_contact_state").val();
+		var city = "Pittsburgh";
+		if($("#service_contact_city").val()){
+			city = $("#service_contact_city").val();
+		}
+	  	var state = "PA";
+	  	if($("#service_contact_state").val()){
+	  		state = $("#service_contact_state").val();
+	  	}
 	  	var output = city + ", " + state;
 	  	$("#location-ex").text(output);
 };
 function updatePhone(){
-	var phone = $("#service_contact_phone").val();
+	var phone = "555.555.5555";
+	if($("#service_contact_phone").val()){
+		phone = $("#service_contact_phone").val();
+	}
 	$("#phone-ex").text(phone);
 }
 
@@ -21,40 +47,43 @@ function ready(){
 	// Updating the title
 	$("#service_title").keyup(function(){
 	  	var str = $(this).val();
-	  	$("#title-ex").text(str);
+	  	if(!$(this).val()){
+	  		$("#title-ex").text("Bob's Towing");
+	  	}
+	  	else{
+	  		$("#title-ex").text(str);
+	  	}
 	  });
 
 	  // Updating the category
 	  $("#service_category").keyup(function(){
 	  	var str = $(this).val();
-	  	$("#category-ex").text(str);
+	  	if(!$(this).val()){
+	  		$("#category-ex").text("Auto Service");
+	  	}
+	  	else{
+	  		$("#category-ex").text(str);
+	  	}
 	  });
 
 	  // Updating the tags
-	  $("#cat1").keyup(updateTags);
-	  $("#cat2").keyup(updateTags);
-	  $("#cat3").keyup(updateTags);
-	  $("#cat4").keyup(updateTags);
-	  $("#cat5").keyup(updateTags);
+	  $("#cat1").keyup(updateCategoryTags);
+	  $("#cat2").keyup(updateCategoryTags);
+	  $("#cat3").keyup(updateCategoryTags);
+	  $("#cat4").keyup(updateCategoryTags);
+	  $("#cat5").keyup(updateCategoryTags);
 
 	  // Updating the location
 	  $("#service_contact_city").keyup(updateLocation);//function(){
-	  	//var city = $("#service_contact_city").val();
-	  	//var state = $("#service_contact_state").val();
-	  	//var output = city + ", " + state;
-	  	//$("#location-ex").text(output);
-	  //});
-
 	  $("#service_contact_state").change(updateLocation);//function(){
-	  	//var city = $("#service_contact_city").val();
-	  	//var state = $("#service_contact_state").val();
-	  	//var output = city + ", " + state;
-	  	//$("#location-ex").text(output);
-	  //});
 
 	  // Updating the phone number
 	  $("#service_contact_phone").keyup(updatePhone);
 
+	  // resetting any empty values
+	  // if(!$("service_title").val() && !$("service_title").is(":focus")){
+	  // 	$("service_title").text("Bob's Towing");
+	  // }
 };
 
 
